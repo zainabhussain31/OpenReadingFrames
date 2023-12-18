c14b666addab041fe262af6c44cbc4de70a20a6a
Mehak Rafiq <mehak@sines.nust.edu.pk>
2023-12-18 12:35:59 +0500

    Update and rename ORF.py to orf_finder.py

---
+++ orf_finder.py
@@ -0,0 +1,17 @@
+# Don't change the Libraries
+import os
+import sys
+
+#Your Code Here
+class ORFFinder:
+    def __init__
+    # Correctly initialize class
+
+    def read_fasta_file   # Implement code to read fasta file and return sequences
+
+    def find_longest_orf  # Implement code to find the longest DNA ORF in a given sequence
+
+    def process_sequences #Implement code to process all sequences and find the overall longest DNA ORF
+        pass
+
+def main # initialize to let various arguments pass
